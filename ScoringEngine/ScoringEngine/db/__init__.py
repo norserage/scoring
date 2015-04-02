cbb8bf5d27b3312afb4a4540d04fefcc7d7bb895
Brandon Warner <thebmw@thebmw.com>
2015-04-02 04:45:13 -0400

    fixed db

--- ScoringEngine/ScoringEngine/db/__init__.py
+++ ScoringEngine/ScoringEngine/db/__init__.py
@@ -1,11 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-<<<<<<< HEAD
 #engine = create_engine('mysql://scoring:scoring@10.151.9.10/scoring')
 engine = create_engine('postgresql://scoring:scoring@10.151.9.11/scoring')
-=======
-engine = create_engine('mysql://scoring:scoring@10.151.9.11/scoring')
->>>>>>> b362ea7536dc0f626bacde5432b7c89e894345ef
 Session = sessionmaker(bind=engine)
 session = Session()
