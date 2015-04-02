2797ddc5b1e4f9cf1225c54e50b34f81d72bb0a3
Brandon Warner <thebmw@thebmw.com>
2015-04-02 04:42:58 -0400

    Changed to postgres

--- ScoringEngine/ScoringEngine/db/__init__.py
+++ ScoringEngine/ScoringEngine/db/__init__.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql://scoring:scoring@10.151.9.10/scoring')
+#engine = create_engine('mysql://scoring:scoring@10.151.9.10/scoring')
+engine = create_engine('postgresql://scoring:scoring@10.151.9.11/scoring')
 Session = sessionmaker(bind=engine)
 session = Session()
