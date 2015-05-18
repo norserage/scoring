3b4a9f41b80948054c3c0ddc351ebcac1f7a3e88
Brandon Warner <thebmw@thebmw.com>
2015-05-18 10:44:01 -0400

    Started working on making things easy

--- ScoringEngine/ScoringEngine/db/__init__.py
+++ ScoringEngine/ScoringEngine/db/__init__.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from ScoringEngine.conf import conf
+import tables
 import pprint
 #engine = create_engine('mysql://scoring:scoring@10.151.9.10/scoring')
 
 pprint.pprint(conf)
 engine = create_engine(conf['database'])
 Session = sessionmaker(bind=engine)
+
+def createUser(name,username,password,team,group):
+    session = Session()
+    user = tables.User()
