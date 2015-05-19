ff7ef681e856b8ad961c1798eec27802df0a8156
Brandon Warner <thebmw@thebmw.com>
2015-05-19 15:12:41 -0400

    trying somthing stupid

---
+++ ScoringEngine/daemon.py
@@ -0,0 +1,19 @@
+"""
+This script runs the ScoringEngine application using a development server.
+"""
+
+# We did not loose the bug!!!!!!!!!!
+'''
+       / .'
+ .---. \/
+(._.' \()
+ ^"""^"
+bug
+'''
+
+import ScoringEngine
+import os
+
+if __name__ == '__main__':
+    os.fork()
+    ScoringEngine.main()
