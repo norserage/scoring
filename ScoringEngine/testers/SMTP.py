7ec93d790d1b2c665ccdc85c515ee9f850385d01
Brandon Warner <thebmw@thebmw.com>
2015-10-02 11:16:14 -0400

    FIxed SMTP

--- ScoringEngine/testers/SMTP.py
+++ ScoringEngine/testers/SMTP.py
@@ -17,12 +17,9 @@ def test(server, service, event):
         conf = utils.getServiceConfig(session, service, server.team)
         user = utils.getRandomUser(session, conf['passdb'])
         r = smtp.login(user['user'],user['pass'])
-        if r[0] == 235:
-            to_email = utils.getRandomEmail(session, conf['passdb'])
-            smtp.sendmail(user['email'],to_email,"This is the test ")
-            se.up = True
-        else:
-            se.up = False
+        to_email = utils.getRandomEmail(session, conf['passdb'])
+        smtp.sendmail(user['email'],to_email,"This is the test ")
+        se.up = True
         #se.info = smtp.ehlo_msg
     except Exception as ep:
         se.info = ep.message
