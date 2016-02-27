e52dcd1bba4140086a2de6f9a3f59f74c1b54d81
thebmw <thebmw@thebmw.com>
2016-02-27 14:07:47 -0500

    .

---
+++ ScoringEngine/testers/RDP.py
@@ -0,0 +1,33 @@
+import ScoringEngine.db.tables as tables
+from ScoringEngine.db import Session
+import ScoringEngine.utils as utils
+import ScoringEngine.options
+import json
+from datetime import datetime
+import socket
+
+def test(server, service, event):
+    #raise NotImplementedError();
+    session=Session()
+    se = tables.ScoreEvent()
+    se.serviceid = service.id;
+    se.teamserverid = server.id;
+    se.scoretime = datetime.now()
+    se.eventid = event
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((server.getIP(), service.port))
+        se.up = True
+    except Exception as e:
+        se.info = e.message
+        se.up = False
+    finally:
+        s.close()
+    session.add(se)
+    session.commit()
+    session.close()
+
+def options():
+    return {
+        
+        }
