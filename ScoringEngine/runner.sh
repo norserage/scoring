f09ca1edd6bf1aa8d812c02ec012c7f750c2cdfd
thebmw <thebmw@thebmw.tech>
2018-09-22 21:20:09 -0700

    missed updating runner paths.

--- ScoringEngine/runner.sh
+++ ScoringEngine/runner.sh
@@ -18,11 +18,11 @@ function setup {
     if [ ! -e "/etc/ise/config.json" ]; then
         out $COLOR_YELLOW "Warning" "Missing config file beginning setup"
         out $COLOR_MAGENTA "Setup" "Creating config file"
-        python /app/runserver.py --make-config=/etc/ise/config.json
+        python /runserver.py --make-config=/etc/ise/config.json
         out $COLOR_MAGENTA "Setup" "Creating database"
         PGPASSWORD=$POSTGRES_ENV_POSTGRES_PASSWORD psql -h $POSTGRES_PORT_5432_TCP_ADDR -U postgres -c 'create database ise;'
         out $COLOR_MAGENTA "Setup" "Setting up database"
-        python /app/runserver.py --gen-db
+        python /runserver.py --gen-db
     fi
     echo "Setup Complete"
 }
@@ -32,7 +32,7 @@ function web_start {
 }
 
 function score_start {
-    python /app/runscore.py
+    python /runscore.py
 }
 
 setup
