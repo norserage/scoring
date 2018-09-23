919dddd8f66e36355a5c5d3d937d61591c5576bf
thebmw <thebmw@thebmw.tech>
2018-09-22 20:43:53 -0700

    Trying to get it running.

--- ScoringEngine/runner.sh
+++ ScoringEngine/runner.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
 
+COLOR_DEFAULT="\e[39m"
+COLOR_BLACK="\e[30m"
+COLOR_RED="\e[31m"
+COLOR_GREEN="\e[32m"
+COLOR_YELLOW="\e[33m"
+COLOR_BLUE="\e[34m"
+COLOR_MAGENTA="\e[35m"
+COLOR_CYAN="\e[36m"
+
+
+function out {
+    echo "[$1$2$COLOR_DEFAULT] $3"
+}
+
 function setup {
     if [ ! -e "/etc/ise/config.json" ]; then
-        echo "Missing config file beginning setup"
+        out $COLOR_YELLOW "Warning" "Missing config file beginning setup"
+        out $COLOR_MAGENTA "Setup" "Creating config file"
         python /app/runserver.py --make-config=/etc/ise/config.json
+        out $COLOR_MAGENTA "Setup" "Creating database"
+        PGPASSWORD=$POSTGRES_ENV_POSTGRES_PASSWORD psql -h $POSTGRES_PORT_5432_TCP_ADDR -U postgres -c 'create database ise;'
+        out $COLOR_MAGENTA "Setup" "Setting up database"
         python /app/runserver.py --gen-db
     fi
     echo "Setup Complete"
